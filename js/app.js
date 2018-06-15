351f0c64b4e6ce1334dcf6339b6f6348f1ab825b
Michael Richards <michaelrichards@Michaels-MacBook-Pro.local>
2018-06-15 14:03:29 -0600

    Commit 10 - Improved Homework Answers

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 
+
 for(let i = 0; i < 20; i++)
 {
   console.log(i);
 }
+
+console.log("\n");
 
 for(let i = 0; i < 200; i++)
 {
@@ -11,6 +14,8 @@ for(let i = 0; i < 200; i++)
     console.log(i);
   }
 }
+
+console.log("\n");
 
 for(let i = 0; i < 20; i++)
 {
@@ -21,6 +26,8 @@ for(let i = 0; i < 20; i++)
     Math.random();
   }
 }
+
+console.log("\n");
 
 for(let i = 0; i < 100; i++)
 {
@@ -40,6 +47,8 @@ for(let i = 0; i < 100; i++)
   }
 }
 
+console.log("\n");
+
 const thom = ["Thom", 1000, "Christchurch"];
 const karolin = ["Karolin", 16, "New York"];
 const kristyn = ["Kristyn", 5, "Pittsburgh"];
@@ -50,15 +59,14 @@ karolin[1] = 17;
 matt[2] = "Gotham City";
 kristyn[2] = "Brookyln";
 
-
 const ninjaTurtles = ["Donatello", "Leonardo", "Raphael", "Michaelangelo"];
 
 for(let i = 0; i < ninjaTurtles.length; i++)
 {
-  ninjaTurtles[i].toUpperCase();
-  console.log(ninjaTurtles[i]);
+  console.log(ninjaTurtles[i].toUpperCase());
 }
 
+console.log("\n");
 
 const kristynsCloset = [
   "left shoe",
@@ -91,12 +99,16 @@ const thomsCloset = [
   ]
 ];
 
-const krystynsShoe = kristynsCloset[0];
+const kristynsShoe = kristynsCloset[0];
 kristynsCloset.shift();
 
-thomsCloset[2].push(krystynsShoe);
+thomsCloset[2].push(kristynsShoe);
 
-const krystynOutfit = [[],[],[]];
+console.log("Hey! There's an alien left shoe in Thom's Closet!")
+console.log(thomsCloset);
+console.log("\n");
+
+const kristynOutfit = [[],[],[]];
 const thomOutfit = [[],[],[]];
 
 // pick Krystyn's 3 outfits
@@ -104,39 +116,48 @@ for(let i = 0; i < 3; i++)
 {
   for(let j = 0; j < kristynsCloset.length; j++)
   {
-    krystynOutfit[i].push(kristynsCloset[(i+j) % kristynsCloset.length])
+    kristynOutfit[i].push(kristynsCloset[(i+j) % kristynsCloset.length])
   }
 }
 
 // pick Thom's 3 outfits
 for(let i = 0; i < 3; i++)
 {
-  for(let j= 0; j < (thomsCloset[i]); j++)
+  for(let j = 0; j < (thomsCloset[i].length); j++)
   {
     thomOutfit[i].push(thomsCloset[i][(i+j) % thomsCloset[i].length]);
   }
 }
 
+console.log("Thom's 3 outfits: ");
+console.log(thomOutfit);
+
+console.log("Kristyn's 3 outfits: ");
+console.log(kristynOutfit);
+console.log("\n");
+
+
+console.log("Washing Kristyn's clothes...");
+
 for(let i = 0; i < kristynsCloset.length; i++)
 {
-  console.log(`WHIRR: Now washing ${krystynsCloset[i]}`);
+  console.log(`WHIRR: Now washing ${kristynsCloset[i]}`);
 }
 
-for(let i = 0; i < thomsCloset.length; i++)
-{
-    for(let j = 0; j < thomsCloset[i].length; j++)
-    {
-        console.log(thomsCloset[i][j]);
-    }
-}
+console.log("All Done Washing! YAY!!! \n");
+
+
+
+console.log("\n");
 
 const multiplesSum = [];
-const sum = 0;
+let sum = 0;
 
-for(let i = 0; i < 1000)
+for(let i = 0; i < 10; i++)
 {
-  if((i % 3 == 0) || (i % 5 == 0))
+  if((i % 3 === 0) || (i % 5 === 0))
   {
+    console.log(i);
     multiplesSum.push(i);
     sum += i;
   }
